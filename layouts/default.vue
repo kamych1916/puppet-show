9f23ef6f8af5ce84a215a215f1f2cfe3675b4830
Rakhimov Kamol <34067263+kamych1916@users.noreply.github.com>
2021-05-12 18:48:57 +0500

    add: new plugin - back to top

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-    <div class="container-fluid px-20">
+    <div>
       <Header />
       <slide-y-down-transition :duration="700">
         <Nuxt />
       </slide-y-down-transition>
+      <back-to-top text="Back to top" visibleoffset="0" right="30px">
+        <button class="btn-fill-golden px-10 py-5">
+          <i class="bi bi-arrow-up-circle"></i>
+        </button>
+      </back-to-top>
     </div>
   </div>
 </template>
