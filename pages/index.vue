9f9cba020d86c92a5beead9e2e9f3626f578f932
Rakhimov Kamol <34067263+kamych1916@users.noreply.github.com>
2021-05-13 01:54:40 +0500

    add: new sections in main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <section class="main">
+  <div class="main">
+    <section>
       <div class="slider w-100">
         <hooper
           group="group1"
@@ -74,36 +74,433 @@
         </div>
       </div>
     </section>
-    <section class="d-flex justify-content-center align-items-center p-100">
+    <section class="container py-100 mb-100 mt-50">
       <div style="position: relative">
-        <!-- <div
-          style="position:absolute; z-index:-1; top:-100px; left: -400px; opacity: 0.4"
-        >
-          <img
-            src="/images/backdrop-text.png"
-            width="150%"
-            style="object-fit: cover"
-          />
-        </div> -->
-        <div
-          class="text-center text-cormorant mb-50"
-          style="font-size: 60px; line-height: 60px; font-style: normal;"
-        >
-          Добро пожаловать в <br />
-          Кукольный Театр Рыбинска!
+        <div class="d-flex justify-content-center align-items-center">
+          <div
+            class="text-center text-cormorant text-italic fs-130"
+            style="position:absolute;  margin-top: 220px; line-height: 100px; z-index:-1;  opacity: 0.03"
+          >
+            Мы любим и гордимся <br />
+            нашим Рыбинским
+            <br />
+            театром кукол!
+          </div>
         </div>
-        <div class="text-center mt-50">
-          Рыбинский театр кукол — это настоящий дворец,<br />
-          где живут куклы из любимых сказок. Наш театр кукол известен, <br />как
-          один из первых театров кукол России.
+        <div class=" d-flex justify-content-center align-items-center">
+          <div class="text-center">
+            <div class="text-cormorant mb-50 fs-60">
+              Добро пожаловать в <br />
+              Рыбинский Театр Кукол!
+            </div>
+            <div class="mt-50">
+              Рыбинский театр кукол — это настоящий дворец,<br />
+              где живут куклы из любимых сказок. Наш театр кукол известен,
+              <br />как один из первых театров кукол России.
+            </div>
+            <div
+              class="text-cormorant text-italic mt-50 text-gold fs-28"
+              style="line-height: 0px;"
+            >
+              1932
+            </div>
+          </div>
         </div>
       </div>
     </section>
-    <section class="d-flex justify-content-center " style="height: 100vh; ">
-      <div class="text-cormorant fs-28  ">
-        <div>Афиша</div>
-        <div class="d-flex justify-content-center mt-20">
-          <img src="/images/decor.png" alt="" />
+
+    <section class="py-50" style="background-color: black">
+      <div class="container text-white">
+        <div class="d-flex justify-content-center text-cormorant fs-28 mb-40 ">
+          <div class="pb-20">
+            <div>Афиша</div>
+            <div class="d-flex justify-content-center mt-10">
+              <img src="/images/decor.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div class="news-card cursor">
+          <div class="row d-flex justify-content-center align-items-center">
+            <div class="col-lg my-10">
+              <img
+                src="/images/news-1.jpg"
+                width="100%"
+                style="object-fit:cover"
+              />
+            </div>
+            <div
+              class="col-lg my-10 d-flex justify-content-center align-items-center"
+            >
+              <div
+                class="text-cormorant text-italic fs-130 mr-30"
+                style="color: #dedede;"
+              >
+                06
+              </div>
+              <div>
+                <div class="text-cormorant fs-28">Мая</div>
+                <div>
+                  суббота
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 my-10 d-flex justify-content-center">
+              <div class="text-center">
+                <div class="text-cormorant fs-28">
+                  «ТЕРЕМ-ТЕРЕМОК» 0+
+                </div>
+                <div>
+                  Май 06, 2021
+                </div>
+                <div>
+                  Город Рыбинск
+                </div>
+              </div>
+            </div>
+            <div class="col-lg my-10 d-flex justify-content-center">
+              <button class="btn-fill-golden">подробнее</button>
+            </div>
+          </div>
+        </div>
+        <div class="news-card">
+          <div class="row d-flex justify-content-center align-items-center">
+            <div class="col-lg my-10">
+              <img
+                src="/images/news-2.jpg"
+                width="100%"
+                style="object-fit:cover"
+              />
+            </div>
+            <div
+              class="col-lg my-10 d-flex justify-content-center align-items-center"
+            >
+              <div
+                class="text-cormorant text-italic fs-130 mr-30"
+                style="color: #dedede;"
+              >
+                14
+              </div>
+              <div>
+                <div class="text-cormorant fs-28">Апреля</div>
+                <div>
+                  воскресенье
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 my-10 d-flex justify-content-center">
+              <div class="text-center">
+                <div class="text-cormorant fs-28">
+                  «В стране дорожных знаков»
+                </div>
+                <div>
+                  Апрель 14, 2021
+                </div>
+                <div>
+                  Город Рыбинск
+                </div>
+              </div>
+            </div>
+            <div class="col-lg my-10 d-flex justify-content-center">
+              <button class="btn-fill-golden">подробнее</button>
+            </div>
+          </div>
+        </div>
+        <div class="news-card">
+          <div class="row d-flex justify-content-center align-items-center">
+            <div class="col-lg my-10">
+              <img
+                src="/images/news-3.jpg"
+                width="100%"
+                style="object-fit:cover"
+              />
+            </div>
+            <div
+              class="col-lg my-10 d-flex justify-content-center align-items-center"
+            >
+              <div
+                class="text-cormorant text-italic fs-130 mr-30"
+                style="color: #dedede;"
+              >
+                02
+              </div>
+              <div>
+                <div class="text-cormorant fs-28">Апреля</div>
+                <div>
+                  суббота
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 my-10 d-flex justify-content-center">
+              <div class="text-center">
+                <div class="text-cormorant fs-28">
+                  «Приключения Буратино»
+                </div>
+                <div>
+                  Апрель 02, 2021
+                </div>
+                <div>
+                  Город Рыбинск
+                </div>
+              </div>
+            </div>
+            <div class="col-lg my-10 d-flex justify-content-center">
+              <button class="btn-fill-golden">подробнее</button>
+            </div>
+          </div>
+        </div>
+        <div class="text-center text-cormorant text-italic my-50">
+          <a href="" class="text-gold">Больше афиш..</a>
+        </div>
+      </div>
+    </section>
+
+    <section class="container py-50">
+      <div class="d-flex justify-content-center text-cormorant fs-28 mb-40 ">
+        <div class="pb-20">
+          <div>Новости</div>
+          <div class="d-flex justify-content-center mt-10">
+            <img src="/images/decor.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-6 my-30">
+          <div class="row">
+            <div class="col">
+              <img
+                src="/images/journal-1.jpg"
+                width="100%"
+                class="journal-img"
+              />
+            </div>
+            <div class="col">
+              <div
+                class="p-10 "
+                style="margin-top: -10px; margin-left: -70px; background-color: white"
+              >
+                <div class="d-flex justify-content-between">
+                  <div>
+                    <div class="fs-14">
+                      20/05/2021
+                    </div>
+                  </div>
+                  <div class="fs-14 text-gold">
+                    Спектакль
+                  </div>
+                </div>
+                <div class="text-cormorant text-italic fs-22">
+                  Серебрянное копытце
+                </div>
+              </div>
+              <div class="mt-20 fs-14">
+                В Рыбинском театре кукол идет подготовка к новому спектаклю
+                "Серебряное копытце", премьера которого состоится 22 и 23 мая.
+                «Серебряное копытце» — сказка Павла Петровича Бажова, любимая
+                ...
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 my-30">
+          <div class="row">
+            <div class="col">
+              <img
+                src="/images/journal-2.jpg"
+                width="100%"
+                class="journal-img"
+              />
+            </div>
+            <div class="col">
+              <div
+                class="p-10 "
+                style="margin-top: -10px; margin-left: -70px; background-color: white"
+              >
+                <div class="d-flex justify-content-between">
+                  <div>
+                    <div class="fs-14">
+                      20/05/2021
+                    </div>
+                  </div>
+                  <div class="fs-14 text-gold">
+                    Спектакль
+                  </div>
+                </div>
+                <div class="text-cormorant text-italic fs-22">
+                  Серебрянное копытце
+                </div>
+              </div>
+              <div class="mt-20 fs-14">
+                В Рыбинском театре кукол идет подготовка к новому спектаклю
+                "Серебряное копытце", премьера которого состоится 22 и 23 мая.
+                «Серебряное копытце» — сказка Павла Петровича Бажова, любимая
+                ...
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 my-30">
+          <div class="row">
+            <div class="col">
+              <img
+                src="/images/journal-3.jpg"
+                width="100%"
+                class="journal-img"
+              />
+            </div>
+            <div class="col">
+              <div
+                class="p-10 "
+                style="margin-top: -10px; margin-left: -70px; background-color: white"
+              >
+                <div class="d-flex justify-content-between">
+                  <div>
+                    <div class="fs-14">
+                      20/05/2021
+                    </div>
+                  </div>
+                  <div class="fs-14 text-gold">
+                    Спектакль
+                  </div>
+                </div>
+                <div class="text-cormorant text-italic fs-22">
+                  Серебрянное копытце
+                </div>
+              </div>
+              <div class="mt-20 fs-14">
+                В Рыбинском театре кукол идет подготовка к новому спектаклю
+                "Серебряное копытце", премьера которого состоится 22 и 23 мая.
+                «Серебряное копытце» — сказка Павла Петровича Бажова, любимая
+                ...
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 my-30">
+          <div class="row">
+            <div class="col">
+              <img
+                src="/images/journal-4.jpg"
+                width="100%"
+                class="journal-img"
+              />
+            </div>
+            <div class="col">
+              <div
+                class="p-10 "
+                style="margin-top: -10px; margin-left: -70px; background-color: white"
+              >
+                <div class="d-flex justify-content-between">
+                  <div>
+                    <div class="fs-14">
+                      20/05/2021
+                    </div>
+                  </div>
+                  <div class="fs-14 text-gold">
+                    Спектакль
+                  </div>
+                </div>
+                <div class="text-cormorant text-italic fs-22">
+                  Серебрянное копытце
+                </div>
+              </div>
+              <div class="mt-20 fs-14">
+                В Рыбинском театре кукол идет подготовка к новому спектаклю
+                "Серебряное копытце", премьера которого состоится 22 и 23 мая.
+                «Серебряное копытце» — сказка Павла Петровича Бажова, любимая
+                ...
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="text-center text-cormorant text-italic my-50">
+        <a href="" class="text-gold">Больше новостей..</a>
+      </div>
+    </section>
+
+    <section
+      class="fs-28 text-center  text-cormorant py-50"
+      style="background-color: #f7f7f7;"
+    >
+      <div class="container">
+        Подпишитесь на наш
+        <a href="" class="text-gold" style="text-decoration: underline">
+          instagram
+        </a>
+      </div>
+    </section>
+
+    <section
+      class="py-100"
+      style="background-color: black; border-bottom: 1px solid #242424"
+    >
+      <div class="container">
+        <div class="row">
+          <div class="col-lg">
+            <img
+              src="/images/footer-map.jpg"
+              width="100%"
+              style="object-fit: cover"
+            />
+          </div>
+          <div class="col-lg">
+            <div class="text-cormorant text-italic text-white fs-22 mb-30">
+              Информация
+            </div>
+            <div class="my-20 fs-14" style="color: rgba(255,255,255,0.4)">
+              <i class="bi bi-geo-alt text-gold"></i>
+              г. Рыбинск, ул. Вокзальная, 14
+            </div>
+            <div class="my-20 fs-14" style="color: rgba(255,255,255,0.4)">
+              <i class="bi bi-envelope text-gold"></i>
+              teatr-kukol-rybinsk@yandex.ru
+            </div>
+            <div class="my-20 fs-14" style="color: rgba(255,255,255,0.4)">
+              <i class="bi bi-telephone text-gold"></i>
+              <a href="tel:4855217027" style="color: rgba(255,255,255,0.4)">
+                (4855) 21 - 70 - 27
+              </a>
+            </div>
+          </div>
+          <div class="col-lg">
+            <div class="text-cormorant text-italic text-white fs-22 mb-30">
+              Режим работы кассы:
+            </div>
+            <div class="my-20 fs-14" style="color: rgba(255,255,255,0.4)">
+              понедельник — выходной,
+            </div>
+            <div class="my-20 fs-14" style="color: rgba(255,255,255,0.4)">
+              вторник — пятница 11.00 — 18.00 перерыв 13.00-14.00,
+            </div>
+            <div class="my-20 fs-14" style="color: rgba(255,255,255,0.4)">
+              суббота, воскресенье 9.00-15.00,
+            </div>
+          </div>
+          <div class="col-lg"></div>
+        </div>
+      </div>
+    </section>
+    <section class="py-100" style="background-color: black;">
+      <div class="container d-flex justify-content-center">
+        <div>
+          <div class="d-flex justify-content-center">
+            <i class="bi bi-instagram text-gold px-10"></i>
+            <i class="bi bi-facebook text-gold px-10"></i>
+            <i class="bi bi-twitter text-gold px-10"></i>
+            <i class="bi bi-youtube text-gold px-10"></i>
+          </div>
+          <div class=" text-center text-cormorant text-white mt-20 fs-28">
+            Рыбинский Театр Кукол
+          </div>
+          <div class="fs-12 text-center mt-20">
+            © 2021. Рыбинский театр кукол / Разработано командой
+            <a
+              href="https://mirllex.space"
+              style="color: #212529; text-decoration: underline"
+            >
+              mirllex
+            </a>
+          </div>
         </div>
       </div>
     </section>
@@ -150,6 +547,22 @@ export default {
         color: #fff;
         letter-spacing: 2px;
       }
+    }
+  }
+  .news-card {
+    padding: 50px 0px;
+    border-top: 1px solid #ccc;
+  }
+  .journal-img {
+    object-fit: cover;
+    width: 300px;
+    height: 300px;
+  }
+  @media (max-width: 1200px) {
+    .journal-img {
+      object-fit: cover;
+      width: 150px;
+      height: 150px;
     }
   }
 }
